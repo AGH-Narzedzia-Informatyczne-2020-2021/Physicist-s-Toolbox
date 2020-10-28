f19a9b48a73ec72a9928ee188dce33658622fe45
michał strzeżoń <mstrzezon@gmail.com>
2020-10-28 16:10:21 +0100

    2proba

---
+++ 1/1/1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include "add.h"
+
+using namespace std;
+
+
+int main() {
+    cout << add(1, 2) << endl;
+    cout << SQR(2 + 3) << endl;
+    return 0;
+}
---
+++ 1/1/add.h
@@ -0,0 +1,13 @@
+#ifndef ADD_H
+#define ADD_H
+
+
+int add (int, int);
+
+#ifdef USE_SQR_MACRO
+#define SQR(n) n * n;
+#else
+inline int SQR(int n) { return n * n; }
+#endif
+
+#endif
