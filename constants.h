e3f2f4d9555e2e246f7d8e0eae197b0afced08e0
JACEK\Michaela <miklimek@student.agh.edu.pl>
2020-11-05 11:37:03 +0100

    adding constants header

---
+++ constants.h
@@ -0,0 +1,35 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+constexpr auto lightSpeed{ 2.99e8 };			// speed of light in vacuum (m/s)
+constexpr auto gravityConst{ 6.674e-11 };		// gravitational constant (m^3/kg*s^2)
+constexpr auto planckConst{ 6.626e-34 };		// Planck constant (J*s)
+constexpr auto CoulombConst{ 8.988e9 };			// Coulomb constant (kg*m^3/s^2*C^2)
+
+constexpr auto gasConst{ 8.31 };				// molar gas constant (J/mol*K)
+constexpr auto Avogardo{ 6.02e23 };				// Avogardo constant (1/mol)
+
+constexpr auto electronMass{ 9.109e-31 };		// mass of electron (kg)
+constexpr auto protonMass{ 1.673e-27 };			// mass of proton (kg)
+constexpr auto neutronMass{ 1.675e-27 };		// mass of neutron (kg)
+
+constexpr auto chargeConst{ 1.602e-19 };		// elementary charge (C)
+
+constexpr auto vacuumMagn{ 1.257e-6 };			// vacuum magnetic permeability (N/A^2)
+constexpr auto vacuumElectr{ 8.854e-12 };		// vacuum electric permittivity (F/m)
+
+constexpr auto gasVolume{ 2.241e-4 };			// ideal gas volume constant (m^3/mol)
+
+constexpr auto EarthGrav{ 9.81 };				// acceleration of gravity (m/s^2)
+constexpr auto EarthMass{ 5.98e24 };			// mass of Earth (kg)
+constexpr auto EarthRadius{ 6.38e6 };			// radius of Earth (m)
+constexpr auto EarthYear{ 3.1536e7 };			// Earth's period of orbit (s)
+constexpr auto EarthEscape{ 1.12e4 };			// Earth's escape velocity (m/s)
+constexpr auto EarthAxis{ 1.496e11 };			// Earth's semi-major axis (m)
+
+constexpr auto AstroUnit{ 1.5e11 };				// Astronomical Unit (m)
+
+
+
+
+#endif
