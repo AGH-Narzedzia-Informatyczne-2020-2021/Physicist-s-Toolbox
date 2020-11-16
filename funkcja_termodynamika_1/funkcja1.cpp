7c7fa951f650a9822dca015c76f7984a3f434ad1
Julia-Pl1 <73125115+Julia-Pl1@users.noreply.github.com>
2020-11-16 17:59:14 +0100

    Update funkcja1.cpp

--- funkcja_termodynamika_1/funkcja1.cpp
+++ funkcja_termodynamika_1/funkcja1.cpp
@@ -3,3 +3,30 @@
 #include <string>
 #include <tgmath.h>
 #include "constants.h"
+
+void TheFirstLaw()
+{
+	cout << "The first law of thermodynamics is also known as The Law of Conservation of Energy" << endl;
+	unsigned double q;
+  cout << "Type how much heat was supplied to the system:" << endl;
+  cin >> q;
+	cout << "Type how much work was done on the layout:" << endl;
+  unsigned double w;
+  cin >> w;
+  u=q+w;
+  for(q<0)
+  {
+    cout << "Internal energy decrease in the system is equal %d, u" << endl;
+  }
+  else
+  {
+   cout << "Internal energy increase in the system is equal %d, u" << endl; 
+  }
+
+}
+
+int main ()
+{
+  printf("TheFirstLaw");
+  return: 0;
+}
