b608bdad32395c5ba603ad2a44d066c0236f5ba7
Julia-Pl1 <73125115+Julia-Pl1@users.noreply.github.com>
2020-11-21 23:10:25 +0100

    Update funkcja1.cpp

--- funkcja_termodynamika_1/funkcja1.cpp
+++ funkcja_termodynamika_1/funkcja1.cpp
@@ -7,11 +7,11 @@
 void TheFirstLaw()
 {
   std::cout << "The first law of thermodynamics is also known as The Law of Conservation of Energy" << endl;
-  unsigned double q;
+  double q;
   std::cout << "Type how much heat was supplied to the system:" << endl;
   std::cin >> q;
   std::cout << "Type how much work was done on the layout:" << endl;
-  unsigned double w;
+  double w;
   std::cin >> w;
   u=q+w;
   for(q<0)
