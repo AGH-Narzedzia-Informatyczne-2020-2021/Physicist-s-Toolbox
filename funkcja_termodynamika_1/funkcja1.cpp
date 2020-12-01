b92f5aba56c07821e35ad40f207e24aee804f1cd
Julia-Pl1 <73125115+Julia-Pl1@users.noreply.github.com>
2020-12-01 18:30:05 +0100

    Update funkcja1.cpp

--- funkcja_termodynamika_1/funkcja1.cpp
+++ funkcja_termodynamika_1/funkcja1.cpp
@@ -8,20 +8,20 @@ int main ()
 {
   std::cout << "The first law of thermodynamics is also known as The Law of Conservation of Energy" << std::endl;
   double q;
-  std::cout << "Type how much heat was supplied to the system:" << std::endl;
+  std::cout << "Type how much heat was supplied to the system (in Joules):" << std::endl;
   std::cin >> q;
-  std::cout << "Type how much work was done on the system:" << std::endl;
+  std::cout << "Type how much work was done on the system (in Joules):" << std::endl;
   double w;
   std::cin >> w;
   double u;
   u=q+w;
   if(u<0)
   {
-   std::cout << "Internal energy decrease in the system is equal"<< u << std::endl;
+   std::cout << "Internal energy decrease in the system is equal (in Joules) "<< u << std::endl;
   }
   else
   {
-   std::cout << "Internal energy increase in the system is equal  " << u << std::endl; 
+   std::cout << "Internal energy increase in the system is equal (in Joules) " << u << std::endl; 
   }
 
 }
