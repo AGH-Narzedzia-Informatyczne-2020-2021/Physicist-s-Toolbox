fcd56908e795b9a47f315a8c2d6d5dd6906ea642
JACEK\Michaela <miklimek@student.agh.edu.pl>
2020-12-04 12:09:31 +0100

    changed the looks of kinematics

--- declarations.h
+++ declarations.h
@@ -2,6 +2,9 @@
 #define DECLARATIONS_H
 
 double inputValidator();
+
+void gravitation();
+void gravityChoice();
 void gravitationalForce();
 void potentialEnergy();
 void gravitationalPotential();
@@ -9,8 +12,24 @@ void escapeVelocity();
 void gravitationalAcceleration();
 void workAgainstGravity();
 void latitudeWeight();
-void gravitation();
+
 void kinematics();
+void kinChoice();
+void horizontal_projection();
+void free_fall();
+void circular_motion();
+
 void electrostatics();
+void electroChoice();
+void force();
+void potential();
+void intensity();
+
 void thermodynamics();
+void thermoChoice();
+void firstLaw();
+void idealGasLaw();
+void heat();
+
+
 #endif 
