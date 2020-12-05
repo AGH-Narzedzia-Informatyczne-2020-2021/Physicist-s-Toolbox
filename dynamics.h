a03767cfff5a067480f3112a8f6bfc9199a07345
Michał Strzeżoń <mstrzezon@gmail.com>
2020-12-05 12:53:04 +0100

    dynamics-finished

--- dynamics.h
+++ dynamics.h
@@ -1,5 +1,5 @@
-#ifndef PHYSICIST_S_TOOLBOX_DYNAMICS_H
-#define PHYSICIST_S_TOOLBOX_DYNAMICS_H
+#ifndef PHYSICIST_S_TOOLBOX_DYNAMICS_HPP
+#define PHYSICIST_S_TOOLBOX_DYNAMICS_HPP
 
 void slippery_slope_without_friction();
 void slippery_slope_with_friction();
