03aa49e1f2a397c3525773ebae0807a82480920a
JACEK\Michaela <miklimek@student.agh.edu.pl>
2020-12-08 15:08:46 +0100

    added an option to chose different field

--- Physicist's_Toolbox.cpp
+++ Physicist's_Toolbox.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
 #include "declarations.h"
 
+double inputValidator()
+{
+	double input{};
+	while (true)
+	{
+		std::cin >> input;
+		if (std::cin.fail())
+		{
+			std::cin.clear();
+			std::cin.ignore(32767, '\n');
+			std::cout << "\nInvalid input. Please try again: ";
+		}
+		else
+		{
+			std::cin.ignore(32767, '\n');
+			break;
+		}
+	}
+	return input;
+}
+
 void fieldChoice()
 {
+	std::cout << "Which field of physics do you need help with today?\n\n"
+		<< " - elctrostatics (1)\n"
+		<< " - thermodynamics (2)\n"
+		<< " - gravitation (3)\n"
+		<< " - kinematics (4)\n"
+		<< " - dynamics (5)\n"
+		<< " - optics (6)\n\n"
+		<< "Field: ";
 	int choice{ static_cast<int>(inputValidator()) };
 	std::cout << '\n';
 
@@ -24,6 +53,14 @@ void fieldChoice()
 		std::cout << "---------------------------------------------------------------------------\n";
 		kinematics();
 		break;
+	case 5:
+		std::cout << "---------------------------------------------------------------------------\n";
+		dynamics();
+		break;
+	case 6:
+		std::cout << "---------------------------------------------------------------------------\n";
+		optics();
+		break;
 	default:
 		std::cout << "The number you've chosen does not correspond to any of the existing fields. Please try again: ";
 		fieldChoice();
@@ -33,15 +70,26 @@ void fieldChoice()
 int main()
 {
 	std::cout << "Welcome to Physicist's Toolbox!\n\n"
-		<< "Please remember to use International System of Units (SI)\n" 
-		<< "---------------------------------------------------------------------------\n"
-		<< "Which field of physics do you need help with today?\n\n"
-		<< " - elctrostatics (1)\n"
-		<< " - thermodynamics (2)\n"
-		<< " - gravitation (3)\n"
-		<< " - kinematics (4)\n\n"
-		<< "Field: ";
+		<< "Please remember to use International System of Units (SI)\n"
+		<< "---------------------------------------------------------------------------\n";
 	fieldChoice();
+
+	while (true)
+	{
+		std::cout << "\n---------------------------------------------------------------------------\n";
+		std::cout << "\nDo you wish to calculate anything else? (Y/N): ";
+		char endOrNot;
+		std::cin >> endOrNot;
+		std::cin.ignore(32767, '\n');
+
+		if (endOrNot == 'y' || endOrNot == 'Y')
+		{
+			std::cout << "\n---------------------------------------------------------------------------\n";
+			fieldChoice();
+		}
+		else
+			break;
+	}
 
 	std::cout << "\n---------------------------------------------------------------------------\n";
 	std::cout << "Thank you for using Physicist's Toolbox! Have a nice day!";
